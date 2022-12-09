7715aae6c0338b330afc670ed6b07cd0557daa85
Albinator <albinamivina@gmail.com>
2022-12-09 12:43:23 +0200

    add RandomChar

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,13 @@
 import AppHeader from '../appHeader/AppHeader';
+import RandomChar from '../randomChar/RandomChar';
 
 const App = () => {
     return (
         <div className="app">
             <AppHeader/>
+            <main>
+                <RandomChar/>
+            </main>
         </div>
     )
 }
