8d3c2ba3a8091a2244a63f47055688a478c7a2f2
Albinator <albinamivina@gmail.com>
2022-12-09 12:24:17 +0200

    add AppHeader

---
+++ src/components/app/App.js
@@ -0,0 +1,11 @@
+import AppHeader from '../appHeader/AppHeader';
+
+const App = () => {
+    return (
+        <div className="app">
+            <AppHeader/>
+        </div>
+    )
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/App.js';
+import './style/style.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
