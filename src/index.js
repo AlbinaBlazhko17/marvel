5f242323bb36568d412ead509ca4ba3b7abbd240
Albinator <albinamivina@gmail.com>
2022-12-12 11:52:39 +0200

    add function to update character

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/App.js';
-import MarvelService from './services/MarvelService.js';
+
 import './style/style.scss';
-
-const marvelService = new MarvelService();
-
-marvelService.getAllCharachters().then(res => res.data.results.forEach(item => console.log(item.name)));
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
