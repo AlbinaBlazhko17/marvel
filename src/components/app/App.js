de9c6616937ce5c5937c13db07eaaaaf58b46740
Albinator <albinamivina@gmail.com>
2022-12-20 12:39:00 +0200

    added page404

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,6 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import AppHeader from '../appHeader/AppHeader';
-import MainPage from '../pages/MainPage';
-import ComicsPage from '../pages/ComicsPage';
+import { MainPage, ComicsPage, Page404} from '../pages';
 
 const App = () => {
 
@@ -13,6 +12,7 @@ const App = () => {
                 <Routes> 
                     <Route path='/' element={<MainPage/>}/>
                     <Route path='/comics' element={<ComicsPage/>}/>
+                    <Route path='*' element={<Page404/>}/>
                 </Routes>
                 </main>
             </div>
---
+++ src/components/pages/index.js
@@ -0,0 +1,5 @@
+import MainPage from '../pages/MainPage';
+import ComicsPage from '../pages/ComicsPage';
+import Page404 from '../pages/404';
+
+export {MainPage, ComicsPage, Page404};
